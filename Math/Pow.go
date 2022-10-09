f6a8799c864df6a33a07a47d762e2a0a3a49e380
Synertry <jonny.somrak@gmail.com>
2022-10-09 18:40:16 +0200

    fix Math.IntPow with negative exp Now returning 0

--- Math/Pow.go
+++ Math/Pow.go
@@ -1,13 +1,16 @@
 package Math
 
-// IntPow calculates n to the mth power. Since the result is an int, it is assumed that m is a positive power
-func IntPow(base, exponent int) (result int) {
-	if exponent == 0 {
+// IntPow calculates base to the expth power. Since the result is an int, it is assumed that exp is a positive power
+// returns 0 if m is negative
+func IntPow(base, exp int) int {
+	if exp < 0 {
+		return 0
+	} else if exp == 0 {
 		return 1
 	}
-	result = base
-	for i := 2; i <= exponent; i++ {
+	result := base
+	for i := 2; i <= exp; i++ {
 		result *= base
 	}
-	return
+	return result
 }
